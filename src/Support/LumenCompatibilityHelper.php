4d5563a8f8ea31904b350e53008135b4ce9a3b88
wadakatu <wadakatukoyo330@gmail.com>
2025-07-07 10:02:46 +0900

    LumenCompatibilityHelperを追加

---
+++ src/Support/LumenCompatibilityHelper.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace LaravelPrism\Support;
+
+use Illuminate\Support\Facades\Route;
+
+class LumenCompatibilityHelper
+{
+    /**
+     * LaravelとLumenの差異を吸収
+     */
+    public static function isLumen(): bool
+    {
+        return ! class_exists('Illuminate\\Foundation\\Application') ||
+               str_contains(app()->version(), 'Lumen');
+    }
+
+    /**
+     * ルートパターンを取得
+     */
+    public static function getRoutePatterns(): array
+    {
+        if (self::isLumen()) {
+            // Lumenの場合、デフォルトでAPIルートのみ
+            return config('prism.route_patterns', ['*']);
+        }
+
+        // Laravelの場合
+        return config('prism.route_patterns', ['api/*']);
+    }
+
+    /**
+     * ルートコレクションを取得
+     *
+     * @return mixed
+     */
+    public static function getRoutes()
+    {
+        if (self::isLumen()) {
+            // Lumenの場合
+            $app = app();
+
+            // @phpstan-ignore-next-line
+            return $app->router->getRoutes();
+        }
+
+        // Laravelの場合
+        return Route::getRoutes();
+    }
+}
