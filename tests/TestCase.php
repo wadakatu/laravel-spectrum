d47838503f8ecda50aa4b58d3c77944284ef31c5
wadakatu <wadakatukoyo330@gmail.com>
2025-07-24 21:27:22 +0900

    test: テスト基盤の改善とヘルパートレイト追加
    テスト環境の改善：
    - TestCase: ルート分離のためのisolateRoutes()メソッドを追加
    - TestCase: clearAllRoutes()でルートコレクションを完全にクリア
    - TestHelpers: テスト用のヘルパートレイトを新規作成
      - FormRequest/Controller/Resourceのモック作成メソッド
      - OpenAPIスキーマのアサーションヘルパー
      - パラメータ検証用のユーティリティメソッド
    
    エッジケーステストの追加：
    - NestedArrayValidationTest: 深いネスト構造のバリデーションルールテスト
    - 動的キーを持つ配列、再帰的構造、混合型の検証を網羅

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace LaravelSpectrum\Tests;
 
+use Illuminate\Routing\RouteCollection;
 use LaravelSpectrum\Analyzers\RouteAnalyzer;
 use LaravelSpectrum\Generators\OpenApiGenerator;
 use LaravelSpectrum\SpectrumServiceProvider;
@@ -26,5 +27,34 @@ abstract class TestCase extends Orchestra
         $routes = app(RouteAnalyzer::class)->analyze();
 
         return app(OpenApiGenerator::class)->generate($routes);
+    }
+
+    /**
+     * 完全にルートをクリアする
+     */
+    protected function clearAllRoutes(): void
+    {
+        $router = $this->app['router'];
+
+        // 新しいRouteCollectionで置き換える
+        $newCollection = new RouteCollection;
+
+        // private propertyにアクセスしてルートコレクションを置き換える
+        $reflection = new \ReflectionProperty($router, 'routes');
+        $reflection->setAccessible(true);
+        $reflection->setValue($router, $newCollection);
+
+        // キャッシュもクリア
+        $router->getRoutes()->refreshNameLookups();
+        $router->getRoutes()->refreshActionLookups();
+    }
+
+    /**
+     * 特定のルートのみを分離して設定する
+     */
+    protected function isolateRoutes(callable $routeDefinitions): void
+    {
+        $this->clearAllRoutes();
+        $routeDefinitions();
     }
 }
