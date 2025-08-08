3f77b8cade19a47789121869ef2faa8f9acbc4a4
wadakatu <wadakatukoyo330@gmail.com>
2025-08-08 16:17:54 +0900

    feat: 匿名FormRequestクラスのバリデーションルール検出機能を実装
    - AnonymousClassFindingVisitorにFormRequest継承チェック機能を追加
    - InlineValidationAnalyzerに匿名FormRequestクラスの検出・解析ロジックを追加
    - $request->validate((new class extends FormRequest {...})->rules())パターンに対応
    - extractAnonymousFormRequestValidationメソッドで匿名クラス内のrules、messages、attributesを抽出

--- src/Analyzers/AST/Visitors/AnonymousClassFindingVisitor.php
+++ src/Analyzers/AST/Visitors/AnonymousClassFindingVisitor.php
@@ -10,12 +10,22 @@ class AnonymousClassFindingVisitor extends NodeVisitorAbstract
 {
     private ?Node\Stmt\Class_ $classNode = null;
 
+    private bool $extendsFormRequest = false;
+
     public function enterNode(Node $node)
     {
         // Look for anonymous class nodes in various contexts
         if ($node instanceof Node\Expr\New_ &&
             $node->class instanceof Node\Stmt\Class_) {
             $this->classNode = $node->class;
+
+            // Check if it extends FormRequest
+            if ($node->class->extends) {
+                $extendedClass = $node->class->extends->toString();
+                if (str_ends_with($extendedClass, 'FormRequest')) {
+                    $this->extendsFormRequest = true;
+                }
+            }
 
             return NodeTraverser::STOP_TRAVERSAL;
         }
@@ -26,5 +36,10 @@ class AnonymousClassFindingVisitor extends NodeVisitorAbstract
     public function getClassNode(): ?Node\Stmt\Class_
     {
         return $this->classNode;
+    }
+
+    public function extendsFormRequest(): bool
+    {
+        return $this->extendsFormRequest;
     }
 }
