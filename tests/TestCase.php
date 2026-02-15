4d2f27f420269723c15bac2d6bdcf6d81e5e67ef
wadakatu <wadakatu.dev@gmail.com>
2026-02-15 09:18:21 +0900

    test: set memory limit before testbench boot

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -15,11 +15,11 @@ abstract class TestCase extends Orchestra
 
     protected function setUp(): void
     {
-        parent::setUp();
-
         // Some CI combinations can end up with the PHP default limit (128M) during long runs.
         // Enforce unlimited memory for package tests to match tests/bootstrap.php intent.
         ini_set('memory_limit', '-1');
+
+        parent::setUp();
     }
 
     protected function getPackageProviders($app)
