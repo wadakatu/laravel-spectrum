8936d3fd5560e91bffff9455b0655b51ff1db2d9
wadakatu <wadakatu.dev@gmail.com>
2026-02-16 08:47:26 +0900

    refactor: add strict_types to mock server

--- src/MockServer/MockServer.php
+++ src/MockServer/MockServer.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace LaravelSpectrum\MockServer;
 
 use Workerman\Connection\TcpConnection;
--- src/MockServer/RouteResolver.php
+++ src/MockServer/RouteResolver.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace LaravelSpectrum\MockServer;
 
 use Illuminate\Support\Str;
