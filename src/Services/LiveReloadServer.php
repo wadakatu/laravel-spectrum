5aaf129c45db731214dd6fb3b3079ca9ab9f63fc
wadakatu <wadakatu.dev@gmail.com>
2026-02-16 10:32:24 +0900

    refactor: complete strict_types rollout for remaining src files

--- src/Services/LiveReloadServer.php
+++ src/Services/LiveReloadServer.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace LaravelSpectrum\Services;
 
 use Workerman\Connection\TcpConnection;
--- src/SpectrumServiceProvider.php
+++ src/SpectrumServiceProvider.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace LaravelSpectrum;
 
 use Illuminate\Support\ServiceProvider;
